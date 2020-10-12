98c7478be6ae41de7a48c4c6f5343d2a09988013
Reece McTiernan <rmctiernan@pm.me>
2020-10-12 21:24:25 +0100

    Add has_many associations to game and team

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   validates :room_id, length: { in: 7..20 }, format: { with: /\A[A-Za-z0-9_]{7,}\z/, message: "only allows A-Z a-z 0-9" }
   before_create :create_slug
+  has_many :teams
 
 
   private
