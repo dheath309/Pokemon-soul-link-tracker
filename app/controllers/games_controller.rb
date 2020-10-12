44ff9d4cb29bb974caa5b673a229ed0b65566f38
Reece McTiernan <rmctiernan@pm.me>
2020-10-12 20:54:45 +0100

    Add room id validation for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,6 @@
 class GamesController < ApplicationController
+
+  def index
+    @game = Game.new
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,17 @@
 class Game < ApplicationRecord
-  validates :room_id, length: { in: 7..20 }
+  validates :room_id, length: { in: 7..20 }, format: { with: /\A[A-Za-z0-9_]{7,}\z/, message: "only allows A-Z a-z 0-9" }
+  before_create :create_slug
+
+
+  private
+    def create_slug
+      characters = Array("A".."Z") + Array("a".."z") + Array("0".."9")
+      str = ""
+      for i in 0..7 
+        str += characters.sample
+      end
+      self.room_id = str
+    end
+
+    # TODO Allow params room_id. check for game with room id and if last access/update > threshold, overwrite
 end
