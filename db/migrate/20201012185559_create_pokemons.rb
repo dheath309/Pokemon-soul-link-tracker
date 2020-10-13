06492e4ae82454404252a69c9a99add75186ce8b
Reece McTiernan <rmctiernan@pm.me>
2020-10-13 17:41:27 +0100

    Update pokemon to have defaults of bulbasaur

--- db/migrate/20201012185559_create_pokemons.rb
+++ db/migrate/20201012185559_create_pokemons.rb
@@ -1,8 +1,8 @@
 class CreatePokemons < ActiveRecord::Migration[6.0]
   def change
     create_table :pokemons do |t|
-      t.string :nickname, null: false
-      t.integer :pokedex_id, null: false
+      t.string :nickname, null: false, default: "Bulbasaur"
+      t.integer :pokedex_id, null: false, default: 1
       t.boolean :is_alive, null: false, default: false
       t.references :team, null: false, foreign_key: true
 
