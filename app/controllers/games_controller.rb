300835c2c51ad734a6160ead2079459d57f6c3ec
Reece McTiernan <rmctiernan@pm.me>
2020-10-22 00:35:22 +0100

    Add game creation based on game id url param
    Matching game id url params do update together, but as soon as another
    game id url param is used, all pages will update to the most recent
    update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,4 +7,11 @@ class GamesController < ApplicationController
       @game.save
     end
   end
+
+  def show
+    @game_id = params[:gameId]
+    @game = Game.find_or_create_by(room_id: @game_id)
+    # TODO Handle when a game is not created (too short room id for example)
+    # TODO StimulusReflex updating ALL pages when we just want to update channel that has same room id
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   validates :room_id, length: { in: 7..20 }, format: { with: /\A[A-Za-z0-9_]{7,}\z/, message: "only allows A-Z a-z 0-9" }
-  before_validation :create_slug, on: :create
+  #before_validation :create_slug, on: :create
   has_many :teams, dependent: :delete_all
 
   private
