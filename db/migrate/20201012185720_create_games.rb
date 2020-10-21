da7ce3361b91e7c1715279e22af7c79bfb090337
Reece McTiernan <rmctiernan@pm.me>
2020-10-22 00:08:11 +0100

    Add unique constraint to game room id

--- db/migrate/20201012185720_create_games.rb
+++ db/migrate/20201012185720_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-      t.string :room_id, null: false
+      t.string :room_id, null: false, index: { unique: true }
       t.timestamps
     end
   end
