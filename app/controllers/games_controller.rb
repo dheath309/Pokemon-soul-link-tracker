73ab1f6335e9db231f00cca2a413fce1bf79137b
Reece McTiernan <rmctiernan@pm.me>
2020-11-30 19:40:11 +0000

    Generate room_id on root page and redirect

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,20 @@
 class GamesController < ApplicationController
   require "poke-api-v2"
 
+  def index
+    @game = Game.new
+    room_id = @game.create_slug
+    redirect_to "/#{room_id}"
+  end
+
   def show
     @game_id = params[:gameId]
+    if @game_id.length < 7
+      # TODO game id too short, error page
+      return
+    end
     @game = Game.includes(:teams).find_or_create_by(room_id: @game_id)
     # TODO Handle when a game is not created (too short room id for example)
-    #
-    #
-
     
     @all_pokemon = Rails.cache.read("all_pokemon")
     if @all_pokemon.nil?
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,15 +3,14 @@ class Game < ApplicationRecord
   #before_validation :create_slug, on: :create
   has_many :teams, dependent: :delete_all
 
-  private
-    def create_slug
-      characters = Array("A".."Z") + Array("a".."z") + Array("0".."9")
-      str = ""
-      for i in 0..7 
-        str += characters.sample
-      end
-      self.room_id = str
+  def create_slug
+    characters = Array("A".."Z") + Array("a".."z") + Array("0".."9")
+    str = ""
+    for i in 0..7 
+      str += characters.sample
     end
+    return str
+  end
 
     # TODO Allow params room_id. check for game with room id and if last access/update > threshold, overwrite
 end
