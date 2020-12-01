12eca7437626f543c46e3aa78068729767337ab0
Reece McTiernan <rmctiernan@pm.me>
2020-12-01 21:03:42 +0000

    Update pokedex id validation on pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon < ApplicationRecord
   belongs_to :team, touch: true
   validates :nickname, length: { minimum: 1 }
-  validates :pokedex_id, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 893} # TODO NOTE This value may need changed 
+  validates :pokedex_id, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 898} # TODO Will need updated when new pokemon are available 
   has_many :links, class_name: "Link", foreign_key: "pokemon1_id"
   has_many :linked_pokemon, through: :links, source: "pokemon2"
 
